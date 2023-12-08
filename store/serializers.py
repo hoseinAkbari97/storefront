f06fca08d78716425c1c57df59d4104276c34d3a
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-12-08 23:15:05 +0330

    Returning Images from the API

--- store/serializers.py
+++ store/serializers.py
@@ -11,13 +11,26 @@ class CollectionSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'products_count']
 
     products_count = serializers.IntegerField(read_only=True)
+    
+    
+class ProductImageSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return ProductImage.objects.create(product_id=product_id,
+                                           **validated_data)
+    
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'slug', 'inventory',
-                  'unit_price', 'price_with_tax', 'collection']
+                  'unit_price', 'price_with_tax', 'collection', 'images']
 
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
@@ -173,12 +186,3 @@ class CreateOrderSerializer(serializers.Serializer):
             return order
 
 
-class ProductImageSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        product_id = self.context['product_id']
-        return ProductImage.objects.create(product_id=product_id,
-                                           **validated_data)
-    
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'image']
--- store/views.py
+++ store/views.py
@@ -16,7 +16,7 @@ from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializ
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
