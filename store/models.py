56cdde6e2bccd856685c3b5a3f74feffb50b6bc4
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-12-09 14:55:29 +0330

    Validating Uploaded Files

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
+from .validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -44,8 +45,11 @@ class Product(models.Model):
         
         
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(
+        upload_to='store/images',
+        validators=[validate_file_size])
 
 
 class Customer(models.Model):
