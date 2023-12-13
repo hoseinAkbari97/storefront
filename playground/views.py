769a910c864763ca63bce747294b9c5af921228a
hossein16akbari@gmail.com <hossein16akbari@gmail.com>
2023-12-13 19:23:13 +0330

    Sending Emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,14 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
+    try:
+        send_mail(
+            'subject', 'message', 'info@moshbuy.com',
+            ['abc@moshbuy.com']
+        )
+    except BadHeaderError:
+        pass
+    
     return render(request, 'hello.html', {'name': 'Mosh'})
