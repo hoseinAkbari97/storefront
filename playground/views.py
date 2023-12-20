aa2817d50e161d465fc3cda51dddcc831ec0e49f
hossein1618 <ho3inrapfa@yahoo.com>
2023-12-20 21:53:18 +0330

    Creating and Executing Tasks

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,7 @@
-from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
 
 
 def say_hello(request):
-    try:
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mosh'}
-        )
-        message.send(['abc@moshbuy.com'])
-    except BadHeaderError:
-        pass
-    
+    notify_customers.delay('Hello')
     return render(request, 'hello.html', {'name': 'Mosh'})
