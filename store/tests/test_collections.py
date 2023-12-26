8ef22aad8f5c8492b44e29f353fd8b4720474658
hossein1618 <ho3inrapfa@yahoo.com>
2023-12-26 22:05:00 +0330

    Running Your Test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.test import APIClient
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
