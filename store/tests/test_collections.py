14b8827a1e1cdc3f2aa2548cc4c89251c0348aee
hossein1618 <ho3inrapfa@yahoo.com>
2023-12-28 21:24:14 +0330

    Authenticating the User

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -11,3 +11,10 @@ class TestCreateCollection:
         response = client.post('/store/collections/', {'title': 'a'})
         
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
