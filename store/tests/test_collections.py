680791c811faf3ed7016a7e55f8fff44273e8e37
hossein1618 <ho3inrapfa@yahoo.com>
2023-12-28 20:39:18 +0330

    Skipping Tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -5,6 +5,7 @@ import pytest
 
 @pytest.mark.django_db
 class TestCreateCollection:
+    @pytest.mark.skip
     def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'a'})
