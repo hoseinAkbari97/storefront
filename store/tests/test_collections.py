d2abd11b63ef91e8601d05ab4790b47a96896057
hossein1618 <ho3inrapfa@yahoo.com>
2023-12-29 13:32:23 +0330

    Single or Multiple Assertion

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
@@ -11,6 +12,7 @@ class TestCreateCollection:
         response = client.post('/store/collections/', {'title': 'a'})
         
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
         
     def test_if_user_is_not_admin_return_403(self):
         client = APIClient()
@@ -18,3 +20,21 @@ class TestCreateCollection:
         response = client.post('/store/collections/', {'title': 'a'})
         
         assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+        
+    def test_if_data_is_invalid_return_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+        
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+        
+        
+    def test_if_data_is_valid_return_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+        
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
