cd2dcb8df9deb987d8e91134a8ad9ec6615f9878
hossein1618 <ho3inrapfa@yahoo.com>
2024-01-03 11:25:07 +0330

    Correcting the Tests

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -4,10 +4,10 @@ import pytest
 
 @pytest.fixture
 def api_client():
-    return APIClient
+    return APIClient()
 
 @pytest.fixture
 def authenticate(api_client):
     def do_authenticate(is_staff=False):
-        return api_client.force_authenticate(user=User, is_staff=is_staff)
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
     return do_authenticate
--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
+from model_bakery import baker
+from store.models import Collection
+from conftest import api_client
 
 @pytest.fixture
 def create_collection(api_client):
@@ -19,7 +22,7 @@ class TestCreateCollection:
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
     
         
-    def test_if_user_is_not_admin_return_403(self, authenticate, api_client, create_collection):
+    def test_if_user_is_not_admin_return_403(self, authenticate, create_collection):
         
         authenticate(is_staff=False)
         
@@ -28,7 +31,7 @@ class TestCreateCollection:
         assert response.status_code == status.HTTP_403_FORBIDDEN
     
         
-    def test_if_data_is_invalid_return_400(self, authenticate, api_client, create_collection):
+    def test_if_data_is_invalid_return_400(self, authenticate, create_collection):
         
         authenticate(is_staff=True)
         
@@ -38,7 +41,7 @@ class TestCreateCollection:
         assert response.data['title'] is not None
         
         
-    def test_if_data_is_valid_return_201(self, authenticate, api_client, create_collection):
+    def test_if_data_is_valid_return_201(self, authenticate, create_collection):
         
         authenticate(is_staff=True)
         
@@ -46,3 +49,19 @@ class TestCreateCollection:
         
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+    
+
+@pytest.mark.django_db   
+class TestRetrieveCollection:
+    
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+        
+        response = api_client.get(f'/store/collections/{collection.id}/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
