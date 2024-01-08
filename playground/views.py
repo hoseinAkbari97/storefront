0ddaf9a6a854533b2c87a42e6828c7bdc8ad4c70
hossein1618 <ho3inrapfa@yahoo.com>
2024-01-08 21:20:15 +0330

    Simulating a Slow API

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .tasks import notify_customers
+import requests
 
 
 def say_hello(request):
-    notify_customers.delay('Hello')
+    requests.get('https://httpbin.org/delay/2')
     return render(request, 'hello.html', {'name': 'Mosh'})
