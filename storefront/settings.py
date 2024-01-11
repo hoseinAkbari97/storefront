d44029bdd3cd4f85c55962efdf7fc1607315a652
hossein1618 <ho3inrapfa@yahoo.com>
2024-01-12 00:42:59 +0330

    Serving Static Assets

--- storefront/settings.py
+++ storefront/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
